5d4cd25fae796ebf32c2504b211e76ed61bed290
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2021-04-19 17:10:49 +0200

    Commit for push

---
+++ app.py
@@ -0,0 +1,20 @@
+import flask, time, datetime
+timer = time.time()
+    
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    try:
+        timed = float(time.time() - timer)
+        todestroy = str((timed - timer)).rindex(".") + 2
+        print(round(timed, int(todestroy)))
+    
+        return "Time: " + str(round(timed, 2))
+    except:
+        return "Programma terminato con codice di errore 13. Errore: eccesso di cringe"
+
+
+app.run(host="0.0.0.0", port=8080)
